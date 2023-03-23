3b3cf22838ea49810c4d2f286c0a2652f16307e9
kristof.lauber <kristof.lauber@smp.hu>
2023-03-23 20:52:03 +0100

    created and set up the function app and cosmos db from de portal

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,22 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
+});
 
-const functionApi = '';
+
+const functionApi = 'https://resumecounterapi.azurewebsites.net/api/ResumeCounter?code=kgynC6WXGZOVDxyP0tKp84wMPvUqgYxWDk9mUB1DBBppAzFuwgoRig==';
 
 const getVisitCount = () => {
-    let count = 2;
-    fetch(functionApi).then(response => {
-        return response.json
-    }).then(response =>{
+    let count = 30;
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
         console.log("Website called function API.");
-        count = response.count
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
+        count = response;
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
         console.log(error);
-    });
+      });
     return count;
 }
